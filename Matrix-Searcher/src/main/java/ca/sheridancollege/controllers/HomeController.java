295a89f5b2940499aa50278a9d3ae542659e9fc3
markzarak <zarak@sheridancollege.ca>
2020-11-01 17:44:59 -0500

    Improved string search algorithm

--- Matrix-Searcher/src/main/java/ca/sheridancollege/controllers/HomeController.java
+++ Matrix-Searcher/src/main/java/ca/sheridancollege/controllers/HomeController.java
@@ -23,8 +23,10 @@ import ca.sheridancollege.database.DatabaseAccess;
 @Controller
 public class HomeController {
 
+	// An empty 2D array that will hold the users randomly generated letters for each round
 	char[][] squareMatrix;
 
+	// Database access for CRUD operations on user information
 	@Autowired
 	private DatabaseAccess ds;
 
@@ -42,7 +44,7 @@ public class HomeController {
 	@GetMapping("/play")
 	public String doStartGame(Model model, @ModelAttribute User user, @RequestParam int length) {
 
-		// Create 2D matrix based on user input
+		// Create 2D array based on user input
 		squareMatrix = doSquareMatrix(model, user, length);
 
 		// Add player info to database to track scores
@@ -55,15 +57,17 @@ public class HomeController {
 	@GetMapping("/score")
 	public String doShowScore(Model model, @ModelAttribute User user, @RequestParam String wordList) {
 
-		int totalWordsFound = 0;
+		// Split String into separate words and track number found during search	
 		List<String> wordToSearch = Arrays.asList(wordList.trim().split("\\s*,\\s*"));
-
-		// Search each word entered by user
+		int totalWordsFound = 0;
+		
+		// Begin search for each word
 		for (String word : wordToSearch) {
+			System.out.println("*****************************word: " + word + "*****************************");
 			totalWordsFound += searchForMatch(word);
 		}
 
-		// Calculate score and add to database
+		// Save score to database
 		int score = totalWordsFound;
 		ds.setScore(score);
 		System.out.println("Score: " + score);
@@ -71,13 +75,13 @@ public class HomeController {
 		return "score.html";
 	}
 
-	// Create 2D matrix filled with random characters
+	// Create array filled with random characters
 	public char[][] doSquareMatrix(Model model, @ModelAttribute User user, @RequestParam int length) {
 
-		// Create empty 2D array based on user input
+		// Create empty array based on user input
 		char[][] squareMatrix = new char[length][length];
 
-		// Fill 2D array with random letters of the English alphabet
+		// Fill array with random letters of the English alphabet
 		for (int floor = 0; floor < squareMatrix.length; ++floor) {
 			for (int room = 0; room < squareMatrix[0].length; ++room) {
 				// Randomly add letter by incrementing char value
@@ -95,17 +99,18 @@ public class HomeController {
 		return squareMatrix;
 	}
 
-	// Search 2D matrix for matching strings
+	// Search squareMatrix for matching strings
 	public int searchForMatch(String word) {
 
 		int wordsFound = 0;
-		// Loop through characters in squareMatrix
+		// Loop through all characters in array squareMatrix
 		for (int floor = 0; floor < squareMatrix.length; ++floor) {
 			for (int room = 0; room < squareMatrix[0].length; ++room) {
-				// Initiate search on first character, if matched check the remaining characters
+				// Search for first character of the word, if matched check for remaining characters
+				System.out.println("looking for:" + word + " from :[" + (floor) + "], room:[" + room + "]");
 				if (squareMatrix[floor][room] == word.charAt(0) && wordMatch(squareMatrix, floor, room, 1, word)) {
 					wordsFound++;
-					System.out.println(word + " has been found, score +" + wordsFound + "\n");
+					System.out.println(word + " successfully found at floor:[" + floor + "], room:[" + room + "]");
 				}
 			}
 		}
@@ -113,114 +118,122 @@ public class HomeController {
 		return wordsFound;
 	}
 	
-	// Returns true if there is a matching String in the 2D matrix
-	public boolean wordMatch(char[][] squareMatrix, int floor, int room, int index, String word) {
-		
-		System.out.println("\nfound: " + word.charAt(0) + " at index 0");
-		System.out.println("looking for remaining letters: " + word);
+	// Returns true if there is a matching String in the 2D array squareMatrix
+	public boolean wordMatch(char[][] squareMatrix, int floor, int room, int indexOfSearch, String word) {
+		System.out.println("\nfound first character match for : " + word.charAt(0) + " from " + word);
 
-		boolean found = false;
-		index = 1;
+		boolean wordFound = false;
+		indexOfSearch = 1;
 		int i = floor;
 		int j = room;
-		// Search vertical down with 2D matrix boundary check
-		while (index != word.length() && !(i < 0 || i >= squareMatrix.length || j < 0 || j >= squareMatrix[i].length)) {
-			System.out.println("\n>Search vertical down");
+		
+		// Search vertical rows down with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i + 1 < squareMatrix.length) {
+			System.out.println("\n>>Search vertical down i+1: " + (i+1) + " squareMatrix.length: " + squareMatrix.length);
 			
-			System.out.println(">looking for:" + word.charAt(index) + " in floor:" + floor + ", room:" + room);
-			System.out.println(">squareMatrix has:" + squareMatrix[i + 1][j]);
-			boolean matchFound = word.charAt(index) == squareMatrix[i + 1][j];
-			System.out.println(">matchFound:" + matchFound);
+//			System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + (i+1) + "], room:[" + j + "], squareMatrix has:" + squareMatrix[i + 1][j]);
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i + 1][j];
+//			System.out.println(">>matchFound:" + matchFound);
 			
 			if (matchFound) {
-				System.out.println(">>floor: " + i + " #Index: " + index + " added letter: " + word.charAt(index));
 				i++;					
-				index++;
+				indexOfSearch++;
+//				System.out.println(">>MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 			} else {
-				index = 1;
+//				System.out.println(">>NO MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
+				indexOfSearch = 1;
 				i = floor;
-				j = room;
+//				System.out.println(">>RESET: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
 		}
 		
-		// Search vertical up with 2D matrix boundary check
-		while (index != word.length() && !(i < 0 || i >= squareMatrix.length || j < 0 || j >= squareMatrix[i].length)) {
-			System.out.println("\n>Search vertical up");
+		// Search vertical rows up with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i - 1 >= 0) {
+			System.out.println("\n>>Search vertical up i-1: " + (i-1));
 			
-			System.out.println(">looking for:" + word.charAt(index) + " in floor:" + floor + ", room:" + room);
-			System.out.println(">squareMatrix has:" + squareMatrix[i - 1][j]);
-			boolean matchFound = word.charAt(index) == squareMatrix[i - 1][j];
-			System.out.println(">matchFound:" + matchFound);
+			// Returns true if the next character of the search string is a match
+//			System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + (i-1) + "], room:[" + j + "], squareMatrix has:" + squareMatrix[i - 1][j]);
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i - 1][j];
+//			System.out.println(">>matchFound:" + matchFound);
 			
 			if (matchFound) {
-				System.out.println(">>floor: " + i + " #Index: " + index + " added letter: " + word.charAt(index));
+				// Increment search
 				i--;					
-				index++;
+				indexOfSearch++;
+//				System.out.println(">>MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 			} else {
-				index = 1;
+//				System.out.println(">>NO MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
+				// Reset search
+				indexOfSearch = 1;
 				i = floor;
-				j = room;
+//				System.out.println(">>RESET: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
 		}
 		
 		// Search horizontal right with 2D matrix boundary check
-		while (index != word.length() && !(i < 0 || i >= squareMatrix.length || j < 0 || j >= squareMatrix[i].length)) {
-			System.out.println("\n>Search  horizontal right");
+		while (indexOfSearch != word.length() && j + 1 < squareMatrix[i].length) {
+			System.out.println("\n>>Search horizontal right j+1: " + (j+1) + " squareMatrix[i].length: " + squareMatrix[i].length);
 			
-			System.out.println(">looking for:" + word.charAt(index) + " in floor:" + floor + ", room:" + room);
-			System.out.println(">squareMatrix has:" + squareMatrix[i][j + 1]);
-			boolean matchFound = word.charAt(index) == squareMatrix[i][j + 1];
-			System.out.println(">matchFound:" + matchFound);
+//			System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + i + "], room:[" + (j+1) + "], squareMatrix has:" + squareMatrix[i][j + 1]);
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i][j + 1];
+//			System.out.println(">>matchFound:" + matchFound);
 			
 			if (matchFound) {
-				System.out.println(">>floor: " + i + " #Index: " + index + " added letter: " + word.charAt(index));
 				j++;					
-				index++;
+				indexOfSearch++;
+//				System.out.println(">>MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 			} else {
-				index = 1;
-				i = floor;
+//				System.out.println(">>NO MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
+				indexOfSearch = 1;
 				j = room;
+//				System.out.println(">>RESET: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
 		}
 		
-		// If all matching characters are found by the end of a String, the word is found
-		if (index == word.length()) {
-			System.out.println(">>WINNER...exiting loop<<");
-			found = true;
-		}
-
 		// Search horizontal left with 2D matrix boundary check
-		while (index != word.length() && !(i < 0 || i >= squareMatrix.length || j < 0 || j >= squareMatrix[i].length)) {
-			System.out.println("\n>Search  horizontal left");
+		while (indexOfSearch != word.length() && j - 1 >= 0) {
+			System.out.println("\n>>Search horizontal left j-1: " + (j-1));
 			
-			System.out.println(">looking for:" + word.charAt(index) + " in floor:" + floor + ", room:" + room);
-			System.out.println(">squareMatrix has:" + squareMatrix[i][j - 1]);
-			boolean matchFound = word.charAt(index) == squareMatrix[i][j - 1];
-			System.out.println(">matchFound:" + matchFound);
+//			System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + i + "], room:[" + (j-1) + "], squareMatrix has:" + squareMatrix[i][j - 1]);
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i][j - 1];
+//			System.out.println(">>matchFound:" + matchFound);
 			
 			if (matchFound) {
-				System.out.println(">>floor: " + i + " #Index: " + index + " added letter: " + word.charAt(index));
 				j--;					
-				index++;
+				indexOfSearch++;
+//				System.out.println(">>MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 			} else {
-				index = 1;
-				i = floor;
+//				System.out.println(">>NO MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
+				indexOfSearch = 1;
 				j = room;
+//				System.out.println(">>RESET: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
 		}
 		
+		// Search vertically and diagonally left rows down with 2D matrix boundary check
+		
+		
+		// Search vertically and diagonally right rows down with 2D matrix boundary check
+		
+		
+		// Search horizontally and diagonally left rows down with 2D matrix boundary check
+		
+		
+		// Search horizontally and diagonally right rows down with 2D matrix boundary check
+		
+		
 		// If all matching characters are found by the end of a String, the word is found
-		if (index == word.length()) {
-			System.out.println(">>WINNER...exiting loop<<");
-			found = true;
+		if (indexOfSearch == word.length()) {
+			System.out.println("!!!!!WINNER!!!!!");
+			wordFound = true;
 		}
 		
-		System.out.println("Returning value: " + found);
-		return found;
+		System.out.println("Returning value: " + wordFound);
+		return wordFound;
 	}
 
 }
--- Matrix-Searcher/src/main/java/ca/sheridancollege/database/DatabaseAccess.java
+++ Matrix-Searcher/src/main/java/ca/sheridancollege/database/DatabaseAccess.java
@@ -17,13 +17,18 @@ public class DatabaseAccess {
 	@Autowired
 	NamedParameterJdbcTemplate jdbc;
 	
+	// Add player name and length to database
 	public void addPlayer(String name, int length) {
+		
+		// Use 2D array length as difficulty multiplier
 		int difficulty = length - 4;
 		String query="INSERT INTO `player_scores` VALUES (6, '" + name + "', " + difficulty + ", 0);";
 		jdbc.update(query, new HashMap());
 	}
 	
+	// Calculate and add score to database
 	public void setScore(int score) {
+		
 		String id="SELECT MAX(id)";
 		String query="UPDATE player_scores SET score=" + score + " WHERE `id` = " + id + ";";
 		jdbc.update(query, new HashMap());
