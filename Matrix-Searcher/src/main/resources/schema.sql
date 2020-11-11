1fd548cfd279e2da324a3866d8118ef949a2d490
markzarak <zarak@sheridancollege.ca>
2020-11-11 06:20:10 -0500

    Finalized SQL table

--- Matrix-Searcher/src/main/resources/schema.sql
+++ Matrix-Searcher/src/main/resources/schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `player_scores` (
 	`id` INT NOT NULL AUTO_INCREMENT Primary Key,
-	`name` VARCHAR(30) NOT NULL, 
+	`name` VARCHAR(30), 
 	`length` INT, 
-	`score` INT,
-	`squareMatrixString` VARCHAR(10000)
+	`score` INT DEFAULT 0,
+	`squareMatrixString` VARCHAR(10000),
+	`continuingGame` boolean DEFAULT false
 );
 
-INSERT INTO `player_scores` (name, length, score) VALUES 
-('markzarak', 99, 999),
-('batman', 7, 20),
-('superman', 9, 200),
-('thor', 7, 85),
-('aquaman', 5, 1);
+INSERT INTO `player_scores` (name, length, score, squareMatrixString) VALUES 
+('Mark Zarak', 99, 999, 'pmcppabzpwuvhqekmrrgleqnruczidyqkocogewgegpmcwuveqnrfchtovkh'),
+('Spider-Man', 7, 110, 'eczidyqkocogewgegfcmtovkpmcppabzvvvh'),
+('Iron Man', 20, 741, 'rczidyqkocogewgegfcmtovkhpmcppabzpwuvhqekmrrgleqhhpabzp'),
+('Thor was here', 12, 325, 'tkfbkdsidghomjdgzomhezugbtkfbkdsidgvvvvhezugb'),
+('Aquaman!!', 5, 1, 'zhnqflmyzbrgufzibetnhfdsn');
