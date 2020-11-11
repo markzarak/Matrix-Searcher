d5758ab2a5b61b2c9b722f131abbd03cc8e43e41
markzarak <zarak@sheridancollege.ca>
2020-11-11 06:26:37 -0500

    Added and completed all page mappings.

--- Matrix-Searcher/src/main/java/ca/sheridancollege/controllers/HomeController.java
+++ Matrix-Searcher/src/main/java/ca/sheridancollege/controllers/HomeController.java
@@ -1,7 +1,7 @@
 /*
  * Home Controller
  * 
- * @author: Mark Zarak, Oct 2020
+ * @author: Mark Zarak, Nov 2020
  */
 
 package ca.sheridancollege.controllers;
@@ -30,12 +30,23 @@ public class HomeController {
 	@Autowired
 	private SquareMatrixSearch squareMatrixSearch;
 
-	// Home Page with user registration
+	// Home Page with user registration 
 	@GetMapping("/") // localhost:8080
 	public String goStartGame(Model model) {
+		
 		model.addAttribute("user", new User());
 
 		return "home.html";
+	}
+	
+	// Home Page for registered users
+	@GetMapping("/home")
+	public String goContinueGame(Model model, @ModelAttribute User user) {
+		
+		user.setName(ds.getPlayer().getName());
+		model.addAttribute("user", user);
+
+		return "registered-user-home.html";
 	}
 
 	// Create and display 2D matrix
@@ -49,15 +60,19 @@ public class HomeController {
 		// Add squareMatrix array to model for Thymeleaf access
 		model.addAttribute("squareMatrix", squareMatrix);
 
-		// Add player info to database
-		ds.addPlayer(user);
-
+		// Add new player or update current player in database, depending on user choice
+		if (ds.getPlayer().isContinuingGame()) {
+			ds.updatePlayer(user);
+		} else {
+			ds.addPlayer(user);
+		}
+		
 		return "play.html";
 	}
 
-	// Calculate and display score
+	// Calculate and display results
 	@GetMapping("/score")
-	public String viewScore(Model model, @RequestParam String wordList) {
+	public String viewScore(Model model, @ModelAttribute User user, @RequestParam String wordList) {
 
 		// Split String into separate words using comma delimiter
 		List<String> wordToSearch = Arrays.asList(wordList.trim().split("\\s*,\\s*"));
@@ -68,14 +83,42 @@ public class HomeController {
 			System.out.println("*****************************word: " + word + "*****************************");
 			totalWordsFound += squareMatrixSearch.searchForMatch(word);
 		}
-
-		// Save score to database
-		ds.setScore(totalWordsFound);
 		
-		// Add players to model for Thymeleaf access
-		model.addAttribute("players", ds.getPlayers());
+		// Retrieve 2D array from database
+		char[][] squareMatrix = SquareMatrixCreator.getSquareMatrixFromString(ds.getPlayer().getLength(),ds.getPlayer().getSquareMatrixString());
+		
+		// Save score to database 
+		ds.setScore(totalWordsFound, squareMatrix.length);
+		
+		// Add data to User object
+		user.setScore(ds.getPlayer().getScore());
+		user.setSquareMatrix(squareMatrix);
+		user.setWordList(wordList);
+		user.setTotalWordsFound(totalWordsFound);
+		
+		// Add objects to model for Thymeleaf access
+		model.addAttribute("user", user);	
+		model.addAttribute("players", ds.getTopPlayers());
 
 		return "score.html";
+	}
+	
+	// Continue game
+	@GetMapping("/continuegame")
+	public String continueGame() {
+
+		ds.setContinuingGame(true);
+		
+		return "redirect:/home";
+	}
+	
+	// New game
+	@GetMapping("/newgame")
+	public String newGame() {
+		
+		ds.setContinuingGame(false);
+		
+		return "redirect:/";
 	}
 
 }
