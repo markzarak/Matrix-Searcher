1116a8322bc3240a6afcb9e3529c31879fa6fee9
markzarak <zarak@sheridancollege.ca>
2020-11-11 19:59:56 -0500

    Completed search functionality by improving previous methods and implementing search in diagonal rows

--- Matrix-Searcher/src/main/java/ca/sheridancollege/squarematrix/SquareMatrixSearch.java
+++ Matrix-Searcher/src/main/java/ca/sheridancollege/squarematrix/SquareMatrixSearch.java
@@ -1,3 +1,10 @@
+/*
+ * A class that searches a 2D array for strings in all directions: horizontally, 
+ * vertically, and diagonally. It returns the number of strings found as an integer.
+ * 
+ * @author: Mark Zarak, Nov 2020
+ */
+
 package ca.sheridancollege.squarematrix;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +18,22 @@ public class SquareMatrixSearch {
 	@Autowired
 	private DatabaseAccess ds;
 
-	// Search squareMatrix for matching strings
+	// Search 2D array squareMatrix for matching strings in any direction
 	public int searchForMatch(String word) {
 
+		// Convert search string to upper case to match 2D array
+		word = word.toUpperCase();
+
+		// Retrieve player's 2D array from database
 		char[][] squareMatrix = SquareMatrixCreator.getSquareMatrixFromString(ds.getPlayer().getLength(), ds.getPlayer().getSquareMatrixString());
 
+		// Begin search through array for first character of the string
 		int wordsFound = 0;
-		// Loop through all characters in array squareMatrix
 		for (int floor = 0; floor < squareMatrix.length; ++floor) {
 			for (int room = 0; room < squareMatrix[0].length; ++room) {
-				// Search for first character of the word, if matched check for remaining characters
-				System.out.println("looking for:" + word + " floor:[" + (floor) + "], room:[" + room + "] has "
-						+ squareMatrix[floor][room]);
-				if (squareMatrix[floor][room] == word.charAt(0) && wordMatch(squareMatrix, floor, room, 1, word)) {
-					wordsFound++;
-					System.out.println(word + " successfully found at floor:[" + floor + "], room:[" + room + "]");
+				// If a match is found, check for remaining characters from all directions and return number of strings found
+				if (squareMatrix[floor][room] == word.charAt(0) && wordMatch(squareMatrix, floor, room, word) > 0) {
+					wordsFound += wordMatch(squareMatrix, floor, room, word);
 				}
 			}
 		}
@@ -33,123 +41,193 @@ public class SquareMatrixSearch {
 		return wordsFound;
 	}
 
-	// Returns true if there is a matching String in the 2D array squareMatrix
-	public static boolean wordMatch(char[][] squareMatrix, int floor, int room, int indexOfSearch, String word) {
-		System.out.println("\nfound first character match for : " + word.charAt(0) + " from " + word);
+	// Returns number of matching Strings in the 2D array squareMatrix
+	public static int wordMatch(char[][] squareMatrix, int floor, int room, String word) {
 
-		boolean wordFound = false;
-		indexOfSearch = 1;
+		int wordsFound = 0;
+		int indexOfSearch = 1; // Searches from second character in string
 		int i = floor;
 		int j = room;
 
 		// Search vertical rows down with 2D matrix boundary check
 		while (indexOfSearch != word.length() && i + 1 < squareMatrix.length) {
-			System.out.println(
-					"\n>>Search vertical down");
 
-//				System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + (i+1) + "], room:[" + j + "], squareMatrix has:" + squareMatrix[i + 1][j]);
 			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i + 1][j];
-//				System.out.println(">>matchFound:" + matchFound);
-
+			// Increment character search if match found, otherwise reset values and break
 			if (matchFound) {
 				i++;
 				indexOfSearch++;
-//					System.out.println(">>MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 			} else {
-//					System.out.println(">>NO MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
-				indexOfSearch = 1;
 				i = floor;
-//					System.out.println(">>RESET: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
+				indexOfSearch = 1;
 				break;
 			}
+		}
+
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			i = floor;
+			indexOfSearch = 1;
+			wordsFound++;
 		}
 
 		// Search vertical rows up with 2D matrix boundary check
 		while (indexOfSearch != word.length() && i - 1 >= 0) {
-			System.out.println("\n>>Search vertical up");
 
 			// Returns true if the next character of the search string is a match
-//				System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + (i-1) + "], room:[" + j + "], squareMatrix has:" + squareMatrix[i - 1][j]);
 			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i - 1][j];
-//				System.out.println(">>matchFound:" + matchFound);
-
+			// Increment character search if match found, otherwise reset values and break
 			if (matchFound) {
-				// Increment search
 				i--;
 				indexOfSearch++;
-//					System.out.println(">>MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 			} else {
-//					System.out.println(">>NO MATCH: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
-				// Reset search
 				indexOfSearch = 1;
 				i = floor;
-//					System.out.println(">>RESET: i(floor):" + i + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
+		}
+
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			i = floor;
+			indexOfSearch = 1;
+			wordsFound++;
 		}
 
 		// Search horizontal right with 2D matrix boundary check
 		while (indexOfSearch != word.length() && j + 1 < squareMatrix[i].length) {
-			System.out.println("\n>>Search horizontal right");
 
-//				System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + i + "], room:[" + (j+1) + "], squareMatrix has:" + squareMatrix[i][j + 1]);
 			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i][j + 1];
-//				System.out.println(">>matchFound:" + matchFound);
-
+			// Increment character search if match found, otherwise reset values and break
 			if (matchFound) {
 				j++;
 				indexOfSearch++;
-//					System.out.println(">>MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 			} else {
-//					System.out.println(">>NO MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				indexOfSearch = 1;
 				j = room;
-//					System.out.println(">>RESET: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
+		}
+
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			j = room;
+			indexOfSearch = 1;
+			wordsFound++;
 		}
 
 		// Search horizontal left with 2D matrix boundary check
 		while (indexOfSearch != word.length() && j - 1 >= 0) {
-			System.out.println("\n>>Search horizontal left");
 
-//				System.out.println(">>looking for:" + word.charAt(indexOfSearch) + " from :[" + i + "], room:[" + (j-1) + "], squareMatrix has:" + squareMatrix[i][j - 1]);
 			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i][j - 1];
-//				System.out.println(">>matchFound:" + matchFound);
-
+			// Increment character search if match found, otherwise reset values and break
 			if (matchFound) {
 				j--;
 				indexOfSearch++;
-//					System.out.println(">>MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 			} else {
-//					System.out.println(">>NO MATCH: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				indexOfSearch = 1;
 				j = room;
-//					System.out.println(">>RESET: j(room):" + j + " indexOfSearch: " + indexOfSearch);
 				break;
 			}
 		}
 
-		// Search vertically and diagonally left rows down with 2D matrix boundary check
+		// Search vertically down and diagonally left rows with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i + 1 < squareMatrix.length && j - 1 >= 0) {
 
-		// Search vertically and diagonally right rows down with 2D matrix boundary
-		// check
-
-		// Search horizontally and diagonally left rows down with 2D matrix boundary
-		// check
-
-		// Search horizontally and diagonally right rows down with 2D matrix boundary
-		// check
-
-		// If all matching characters are found by the end of a String, the word is
-		// found
-		if (indexOfSearch == word.length()) {
-			System.out.println("!!!!!WINNER!!!!!");
-			wordFound = true;
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i + 1][j - 1];
+			// Increment character search if match found, otherwise reset values and break
+			if (matchFound) {
+				i++;
+				j--;
+				indexOfSearch++;
+			} else {
+				i = floor;
+				j = room;
+				indexOfSearch = 1;
+				break;
+			}
 		}
 
-		System.out.println("Returning value: " + wordFound);
-		return wordFound;
-	}
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			i = floor;
+			j = room;
+			indexOfSearch = 1;
+			wordsFound++;
+		}
 
+		// Search vertically down and diagonally right rows with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i + 1 < squareMatrix.length && j + 1 < squareMatrix[i].length) {
+
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i + 1][j + 1];
+			// Increment character search if match found, otherwise reset values and break
+			if (matchFound) {
+				i++;
+				j++;
+				indexOfSearch++;
+			} else {
+				i = floor;
+				j = room;
+				indexOfSearch = 1;
+				break;
+			}
+		}
+
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			i = floor;
+			j = room;
+			indexOfSearch = 1;
+			wordsFound++;
+		}
+
+		// Search vertically up and diagonally left rows with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i - 1 >= 0 && j - 1 >= 0) {
+
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i - 1][j - 1];
+			// Increment character search if match found, otherwise reset values and break
+			if (matchFound) {
+				i--;
+				j--;
+				indexOfSearch++;
+			} else {
+				i = floor;
+				j = room;
+				indexOfSearch = 1;
+				break;
+			}
+		}
+
+		// Resets values and increments wordsFound if string was found and contains more than one character
+		if (indexOfSearch == word.length() && word.length() > 1) {
+			i = floor;
+			j = room;
+			indexOfSearch = 1;
+			wordsFound++;
+		}
+
+		// Search vertically up and diagonally right rows with 2D matrix boundary check
+		while (indexOfSearch != word.length() && i - 1 >= 0 && j + 1 < squareMatrix[i].length) {
+
+			boolean matchFound = word.charAt(indexOfSearch) == squareMatrix[i - 1][j + 1];
+			// Increment character search if match found, otherwise reset values and break
+			if (matchFound) {
+				i--;
+				j++;
+				indexOfSearch++;
+			} else {
+				i = floor;
+				j = room;
+				indexOfSearch = 1;
+				break;
+			}
+		}
+
+		// Increments wordsFound if a string of any size was found
+		if (indexOfSearch == word.length()) {
+			wordsFound++;
+		}
+
+		return wordsFound;
+	}
 }
